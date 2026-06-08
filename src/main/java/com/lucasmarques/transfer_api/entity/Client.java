6a8821ea2b925def49b20befef7e207719b82a66
Lucas Marques <lucaamarques2406@gmail.com>
2026-06-08 20:32:30 -0300

    feat: create entities for tables accounts, clients and transfer

---
+++ src/main/java/com/lucasmarques/transfer_api/entity/Client.java
@@ -0,0 +1,30 @@
+package com.lucasmarques.transfer_api.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String cpf;
+
+    @Email
+    @NotBlank
+    @Column(nullable = false)
+    private String email;
+}
---
+++ src/main/java/com/lucasmarques/transfer_api/entity/Transfer.java
@@ -0,0 +1,41 @@
+package com.lucasmarques.transfer_api.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "transfer")
+public class Transfer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "origin_id", referencedColumnName = "id")
+    private Account originAccount;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "destination_id", referencedColumnName = "id")
+    private Account destinationAccount;
+
+    @NotNull
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDateTime dateTransfer;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private StatusTransfer status;
+}
