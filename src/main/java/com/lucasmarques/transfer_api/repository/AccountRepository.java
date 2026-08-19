8230cda0a5c784a4ed62837f11bff6d959ce2d5c
Lucas Marques <lucaamarques2406@gmail.com>
2026-08-19 15:13:22 -0300

    feat: add findByWithLock with pessimistic write lock

--- src/main/java/com/lucasmarques/transfer_api/repository/AccountRepository.java
+++ src/main/java/com/lucasmarques/transfer_api/repository/AccountRepository.java
@@ -1,9 +1,19 @@
 package com.lucasmarques.transfer_api.repository;
 
 import com.lucasmarques.transfer_api.entity.Account;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository<Account, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT a FROM Account a WHERE a.id = :id")
+    Optional<Account> findByIdWithLock(@Param("id") UUID id);
+
 }
